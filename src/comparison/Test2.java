ecde73430c72db2adc240b73c938e8766975a88f
mishgan55 <vorota.mogilev@gmail.com>
2023-07-06 18:39:53 +0300

    Interface Comparable

--- src/comparison/Test2.java
+++ src/comparison/Test2.java
@@ -2,7 +2,6 @@ package comparison;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
