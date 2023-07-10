7908c2f0823b73b3d925b01418df51f689cc92e1
mishgan55 <vorota.mogilev@gmail.com>
2023-07-10 18:42:52 +0300

    solved

--- src/algorithms/ContinuousBackpack.java
+++ src/algorithms/ContinuousBackpack.java
@@ -1,112 +1,69 @@
 package algorithms;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
+
+import java.util.Scanner;
 
 public class ContinuousBackpack {
+    public static void main(String[] args) {
+        new ContinuousBackpack().run();
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    }
+    public void run(){
+        Scanner scanner= new Scanner(System.in);
 
-        int maxStaff;
-        int maxValue;
-
-        ArrayList<Item> objects = new ArrayList<>();
-
-        String[] first=bufferedReader.readLine().split(" ");
-        maxStaff = Integer.parseInt(first[0]);
-        maxValue= Integer.parseInt(first[1]);
-        for (int i = 0; i < maxStaff; i++) {
-            String[] second=bufferedReader.readLine().split(" ");
-            objects.add(new Item(Integer.parseInt(second[0]),Integer.parseInt(second[1])));
+        int capacity = scanner.nextInt();
+        int maxWeight = scanner.nextInt();
+        Item[] items=new Item[capacity];
+        for (int i = 0; i < capacity; i++) {
+            items[i]=new Item(scanner.nextInt(),scanner.nextInt());
         }
-        bufferedReader.close();
+        Arrays.sort(items);
 
-        Collections.sort(objects);
+        double result=0;
 
-        double valuePrice=0.0;
-
-
-        for (Item item:
-             objects) {
-            if (item.size<maxValue) {
-                maxValue -= item.size;
-                valuePrice += item.price;
+        for (Item item : items) {
+            if(item.weight<=maxWeight){
+                maxWeight-=item.weight;
+                result+=item.cost;
             }else {
-                valuePrice+=((double) maxValue/(double)item.size)* item.price;
-                System.out.printf("%.3f",valuePrice);
-                break;
+                result+=(double) item.cost*maxWeight/item.weight;
 
+                break;
 
             }
 
-
         }
+        System.out.println(result);
 
 
 
 
-
-        System.out.println(objects);
 
     }
-
-
 }
-
-
-
-
 class Item implements Comparable<Item>{
-    int price;
-    int size;
+    int cost;
+    int weight;
 
-    public Item(int price, int size) {
-        this.price = price;
-        this.size = size;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public int getSize() {
-        return size;
-    }
-
-    public void setSize(int size) {
-        this.size = size;
-    }
-
-    public int priceForOne(){
-        return price/size;
-    }
-
-    @Override
-    public int compareTo(Item o) {
-        if ((double)this.price/(double)this.size>(double)o.getPrice()/(double)o.getSize()){
-            return -1;
-        }else if ((double)this.price/(double)this.size<(double)o.getPrice()/(double)o.getSize()){
-            return 1;
-        }else {
-            return 0;
-        }
-
+    public Item(int cost, int weight) {
+        this.cost = cost;
+        this.weight = weight;
     }
 
     @Override
     public String toString() {
         return "Item{" +
-                "price=" + price +
-                ", size=" + size +
+                "cost=" + cost +
+                ", weight=" + weight +
                 '}';
+    }
+
+    @Override
+    public int compareTo(Item o) {
+        double r1=(double) this.cost/this.weight;
+        double r2=(double) o.cost/o.weight;
+        return -Double.compare(r1,r2);
     }
 }
