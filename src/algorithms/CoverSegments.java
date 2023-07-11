2b8a76447770c60df5f7e95d11541d44cb1f1fd6
mishgan55 <vorota.mogilev@gmail.com>
2023-07-11 14:46:42 +0300

    Programming problem: cover segments with dots

--- src/algorithms/CoverSegments.java
+++ src/algorithms/CoverSegments.java
@@ -2,9 +2,14 @@ package algorithms;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.Scanner;
+
+/*По данным n отрезкам необходимо найти множество точек минимального размера,
+ для которого каждый из отрезков содержит хотя бы одну из точек.
+В первой строке дано число
+1≤n≤100 отрезков. Каждая из последующих
+n строк содержит по два числа0≤l≤r≤10, задающих начало и конец отрезка. Выведите оптимальное число,
+m точек и сами m точек. Если таких множеств точек несколько, выведите любое из них.*/
 
 public class CoverSegments {
     public static void main(String[] args) {
@@ -22,115 +27,77 @@ public class CoverSegments {
         }
 
 
-
-
-
         Arrays.sort(arrSegments);
 
-        for (Segment arrSegment : arrSegments) {
-            System.out.print(arrSegment);
 
-        }
+        int currentPoint = -1;
+        int count = 0;
 
-
-//        ArrayList<Integer> ints = new ArrayList<>();
-//
-//
-//
-//
-//        for (int i = 0; i < size; i++) {
-//            ints.add(arrSegments[i].getSecondDot());
-//
-//        }
-//        System.out.println(ints);
-//        int ind=0;
-//
-//        for (int i = 0; i < size; i++) {
-//            if (ints.get(i)>=arrSegments[i+1].getFirstDot()){
-//                return;
-//
-//            }else if(ints.get(i)>=arrSegments[i+1].getFirstDot()){
-//                ints.remove(i);
-//            }
-//        }
-//        System.out.println(ints);
+        ArrayList<Integer> integers = new ArrayList<>();
 
 
+        for (int i = 0; i < size; i++) {
 
-
-
-
-
-            ArrayList<Integer> integers = new ArrayList<>();
-
-        int ind=;
-
-            for (int i = 1; i <size-1 ; i++) {
-                if(integers.isEmpty()){
-                    integers.add(arrSegments[0].getSecondDot());
-                }
-            if (arrSegments[i-1].getSecondDot()>=arrSegments[i].getFirstDot()){
-                arrSegments[i].setSecondDot(arrSegments[i-1].getSecondDot());
-
-
-
-            }else{
-                ind++;
-                i--;
-            }
-
-
-
-        }
-
-
-
-
-        }
-    }
-
-    class Segment implements Comparable<Segment> {
-        private int firstDot;
-         private  int secondDot;
-
-        public Segment(int firstDot, int secondDot) {
-            this.firstDot = firstDot;
-            this.secondDot = secondDot;
-        }
-
-        public int getFirstDot() {
-            return firstDot;
-        }
-
-        public void setFirstDot(int firstDot) {
-            this.firstDot = firstDot;
-        }
-
-        public int getSecondDot() {
-            return secondDot;
-        }
-
-        public void setSecondDot(int secondDot) {
-            this.secondDot = secondDot;
-        }
-
-        @Override
-        public String toString() {
-            return "Segment{" +
-                    "firstDot=" + firstDot +
-                    ", secondDot=" + secondDot +
-                    '}';
-        }
-
-        @Override
-        public int compareTo(Segment o) {
-            if (this.secondDot > o.secondDot) {
-                return 1;
-            } else if (this.secondDot < o.secondDot) {
-                return -1;
-            } else {
-                return 0;
+            if (currentPoint < arrSegments[i].getFirstDot()) {
+                count++;
+                currentPoint = arrSegments[i].getSecondDot();
+                integers.add(currentPoint);
             }
         }
+        System.out.println(count);
+        for (Integer integer : integers) {
+            System.out.print(integer + " ");
+
+        }
+
     }
+
+
+}
+
+
+class Segment implements Comparable<Segment> {
+    private int firstDot;
+    private int secondDot;
+
+    public Segment(int firstDot, int secondDot) {
+        this.firstDot = firstDot;
+        this.secondDot = secondDot;
+    }
+
+    public int getFirstDot() {
+        return firstDot;
+    }
+
+    public void setFirstDot(int firstDot) {
+        this.firstDot = firstDot;
+    }
+
+    public int getSecondDot() {
+        return secondDot;
+    }
+
+    public void setSecondDot(int secondDot) {
+        this.secondDot = secondDot;
+    }
+
+    @Override
+    public String toString() {
+        return "Segment{" +
+                "firstDot=" + firstDot +
+                ", secondDot=" + secondDot +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Segment o) {
+        if (this.secondDot > o.secondDot) {
+            return 1;
+        } else if (this.secondDot < o.secondDot) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+}
 
