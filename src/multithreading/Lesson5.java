4df0ea1fb7a42cc5287ffea0ff15a9a1a98e64cc
mishgan55 <vorota.mogilev@gmail.com>
2023-08-19 22:39:13 +0300

    ExecutorService repeated

--- src/multithreading/Lesson5.java
+++ src/multithreading/Lesson5.java
@@ -1,39 +1,38 @@
 package multithreading;
 
-import java.util.concurrent.Executor;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class Lesson5 {
-    public static void main(String[] args) throws InterruptedException {
-        ExecutorService executorService= Executors.newFixedThreadPool(3);
-
-        for (int i = 1; i <= 12; i++)
-            executorService.submit(new Work(i));
-
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        for (int i = 1; i <= 10; i++)
+            executorService.submit(new Workers(i));
         executorService.shutdown();
-
-        executorService.awaitTermination(1, TimeUnit.DAYS);
-
-    }
-
-}
-class Work implements Runnable{
-    int id;
-
-    public Work(int id) {
-        this.id = id;
-    }
-
-
-    @Override
-    public void run() {
-        System.out.println("Work "+id+" completed");
         try {
-            Thread.sleep(5000);
+            executorService.awaitTermination(1, TimeUnit.DAYS);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+}
+class Workers implements Runnable{
+    private int id;
+
+    public Workers(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Work with "+id+" completed");
     }
 }
