beb4310b3a80b11c9714e2881df13697a6b5c065
mishgan55 <vorota.mogilev@gmail.com>
2023-08-19 21:39:21 +0300

    synchronized block repeated

--- src/multithreading/Lesson4.java
+++ src/multithreading/Lesson4.java
@@ -11,78 +11,58 @@ public class Lesson4 {
 
 }
 class Worker{
-    Random random= new Random();
+    Random random=new Random();
+    private final List<Integer> list1=new ArrayList<>();
+    private final List<Integer> list2=new ArrayList<>();
 
-    List<Integer> list1= new ArrayList<>();
-    List<Integer> list2= new ArrayList<>();
+    final Object lock1=new Object();
+    final Object lock2=new Object();
 
-    Object lock1=new Object();
-    Object lock2=new Object();
 
-    public void addList1() throws InterruptedException {
-        synchronized (lock1) {
-            list1.add(random.nextInt(100));
-            Thread.sleep(1);
+    public void main(){
 
-        }
-    }
-
-    public void addList2() throws InterruptedException {
-        synchronized (lock2) {
-            list2.add(random.nextInt(100));
-            Thread.sleep(1);
-
-        }
-    }
-
-    public void work() throws InterruptedException {
-        for (int i = 0; i < 1000; i++) {
-            addList1();
-            addList2();
-
-        }
-
-    }
-
-    public void main() throws InterruptedException {
-
-        long finish = System.currentTimeMillis();
-
-        Thread thread1=new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    work();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        Thread thread2= new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    work();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+        Thread thread1 = new Thread(this::work);
+        Thread thread2 = new Thread(this::work);
 
         thread1.start();
         thread2.start();
-
-        thread1.join();
-        thread2.join();
-
-
-        long start = System.currentTimeMillis();
-
-        System.out.println(start-finish+"ms");
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         System.out.println(list1.size());
         System.out.println(list2.size());
 
+    }
 
+    private void list1Add(){
+        synchronized (lock1){
+        try {
+            Thread.sleep(1);
+            list1.add(random.nextInt(100));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }}
+
+    }
+    private void list2Add(){
+       synchronized (lock2) {
+           try {
+               Thread.sleep(1);
+               list2.add(random.nextInt(100));
+           } catch (InterruptedException e) {
+               e.printStackTrace();
+           }
+       }
+
+    }
+
+    private void work(){
+        for (int i = 0; i < 1000; i++) {
+            list1Add();
+            list2Add();
+        }
     }
 }
