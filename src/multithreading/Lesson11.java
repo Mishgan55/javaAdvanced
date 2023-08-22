ab2dbcf12dc93f3da225e2b31d7bc76c08866961
mishgan55 <vorota.mogilev@gmail.com>
2023-08-22 21:56:21 +0300

    Semaphore repeated

--- src/multithreading/Lesson11.java
+++ src/multithreading/Lesson11.java
@@ -7,59 +7,49 @@ import java.util.concurrent.TimeUnit;
 
 public class Lesson11 {
     public static void main(String[] args) throws InterruptedException {
-
-        Connection connection = Connection.getConnection();
-        ExecutorService executorService= Executors.newFixedThreadPool(100);
-
-        for (int i = 0; i < 100; i++) {
-            executorService.submit(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        connection.work();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-
-
+        Connection connection=Connection.getInstance();
+        ExecutorService executorService=Executors.newFixedThreadPool(50);
+        for (int i = 0; i <50 ; i++) {
+        executorService.submit(()->{
+            try {
+                connection.work();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
         }
         executorService.shutdown();
-        executorService.awaitTermination(1, TimeUnit.DAYS);
-
+        executorService.awaitTermination(1,TimeUnit.DAYS);
 
     }
 }
-class Connection{
-    private static Connection connection=new Connection();
-    private int countConnections;
-    Semaphore semaphore = new Semaphore(10);
 
+class Connection{
+    private static final Connection instance=new Connection();
+    private int countConnections;
+    private final Semaphore semaphore=new Semaphore(5);
     private Connection() {
     }
-
-    public static Connection getConnection(){
-        return connection;
-    }
-
-    public void work() throws InterruptedException {
-        semaphore.acquire();
-        try{
-        doWork();
-        }finally {
-            semaphore.release();
-        }
+    public static Connection getInstance(){
+        return instance;
     }
 
     private void doWork() throws InterruptedException {
         synchronized (this){
             countConnections++;
+            System.out.println(countConnections);
         }
-        System.out.println(countConnections);
-        Thread.sleep(5000);
+        Thread.sleep(2000);
         synchronized (this){
             countConnections--;
+        }
+    }
+    public void work() throws InterruptedException {
+        semaphore.acquire();
+        try {
+            doWork();
+        }finally {
+            semaphore.release();
         }
     }
 }
